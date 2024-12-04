e01e86b42a647fddfedd28646e4d67166fd981f8
tison <wander4096@gmail.com>
2024-12-04 10:26:00 +0800

    build: redirect homepage and upgrade deps
    Signed-off-by: tison <wander4096@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,7 @@
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
+import {Redirect} from "@docusaurus/router";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import React from "react";
 
-import styles from './index.module.css';
-import {JSX} from 'react';
-import Link from '@docusaurus/Link';
-
-function HomepageHeader() {
-  return (
-    <header className={clsx('hero', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">
-          The Most Flexible Database for Developing Applications
-        </h1>
-      </div>
-    </header>
-  );
-}
-
-export default function Home(): JSX.Element {
-  return (
-    <Layout
-      description="ScopeDB is a database for observability data at any scale.">
-      <HomepageHeader/>
-      <main/>
-    </Layout>
-  );
+export default function Home(): React.JSX.Element {
+  return <Redirect to={useBaseUrl("reference/overview")}/>
 }
