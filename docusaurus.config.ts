0c5d7a68fc2fe85d104309542c498a4f8c5aa028
tison <wander4096@gmail.com>
2025-01-23 15:29:04 +0800

    break down docs
    Signed-off-by: tison <wander4096@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -62,6 +62,11 @@ const config: Config = {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
+    docs: {
+      sidebar: {
+        autoCollapseCategories: true,
+      }
+    }
   } satisfies Preset.ThemeConfig,
 
   plugins: [
