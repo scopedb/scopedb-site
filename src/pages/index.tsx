b03a2f0fe51a0ccaacf387fe20ab1be25b7dda87
tison <wander4096@gmail.com>
2025-02-18 09:44:40 +0800

    adjust words
    Signed-off-by: tison <wander4096@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,19 @@ function HomepageBanner(): React.JSX.Element {
   return (
     <div className={styles.scopeBanner}>
       <div className={styles.scopeBannerContent}>
-        <div className={styles.scopeBannerFeature}>10x Performance, 0.1x Cost,100x More Elastic</div>
+        <div className={styles.scopeBannerFeature}>10x Performance, 0.1x Cost, 100x More Elastic</div>
         <h1 className={styles.scopeBannerTitle}>
-          <span className={styles.scopeBannerBrand}>The Future </span>of Cloud Data
+          <span className={styles.scopeBannerBrand}>The Future</span> of Cloud Data Management
         </h1>
         <div className={styles.scopeBannerIntro}>
-          ScopoDB is a cloud-native database engineered to handle semi-structured data like logs and web data, with the
-          capability to manage diverse workloads under massive data volumes.
+          ScopeDB is a database that runs directly on top of any commodity object storage,
+          unleashing the power of cloud elasticity and workload isolation.
+          It is designed explicitly for data workloads with massive writes, any-scale reads,
+          and flexible schema.
         </div>
         <div className={styles.scopeBtns}>
-          <a href="/reference/overview" className={clsx(styles.scopeBtn, styles.scopeBtnPrimary)}>Book a Demo</a>
-          <a href="/reference/overview" className={styles.scopeBtn}>Document</a>
+          <a href="/contact" className={clsx(styles.scopeBtn, styles.scopeBtnPrimary)}>Book a Demo</a>
+          <a href="/blog/manage-observability-data-in-petabytes" className={styles.scopeBtn}>Read More</a>
         </div>
       </div>
       <div className={styles.scopeBannerImgView}>
