317d4035ef91e5d4d21e5418ab20dfd172e4299c
CaiCai <hi@cai-cai.me>
2025-06-08 13:40:16 -0600

    style: update homepage images and position

--- src/components/landing-page/ScopeQLVSSQL/index.tsx
+++ src/components/landing-page/ScopeQLVSSQL/index.tsx
@@ -56,7 +56,7 @@ export default function ScopeQLVSSQL() {
     const [activeTab, setActiveTab] = useState(0);
 
     return (
-        <div className="bg-[#F9F9F9] rounded-[16px] py-[16px] px-[16px] flex flex-col h-[800px] max-w-[720px] ">
+        <div className="bg-[#F9F9F9] rounded-[16px] py-[16px] px-[16px] flex flex-col max-w-[720px] ">
             {/* Tab Navigation */}
             <div
                 className="flex text-[14px] flex-nowrap gap-[24px] overflow-x-auto px-[8px] text-[var(--color-secondary)]">
@@ -73,19 +73,19 @@ export default function ScopeQLVSSQL() {
 
             <div className="mt-[16px] flex flex-col flex-1">
                 {/* SQL Section */}
-                <div className="flex-1 bg-[#FFFFFF] rounded-[12px] p-[20px] mb-[12px]"
+                <div className="flex-1 bg-[#FFFFFF] rounded-[12px] p-[16px] mb-[12px]"
                      style={{
                          boxShadow: "0px 0px 8px 8px rgba(0, 0, 0, 0.02);"
                      }}>
                     <div className="pb-[14px]">
                         <h3 className="text-[rgba(0,0,0,0.40)] text-[13px]">SQL</h3>
                     </div>
-                    <div className="font-[14px] overflow-x-auto max-w-[766px]">
+                    <div className="font-[14px] overflow-x-auto w-full">
                         <ShikiHighlighter
                             theme="min-light"
                             language="sql"
                             showLanguage={false}
-                            className='text-[14px] whitespace-pre-wrap pr-0 pl-0'
+                            className='text-[14px] whitespace-pre-wrap p-0'
 
                         >
                             {categories[activeTab].sql.trim()}
@@ -101,12 +101,13 @@ export default function ScopeQLVSSQL() {
                     <div className="pb-[14px]">
                         <h3 className="text-[rgba(0,0,0,0.40)] text-[13px]">ScopeQL</h3>
                     </div>
-                    <div className="font-[14px] overflow-x-auto ">
+                    <div className="font-[14px] overflow-x-auto w-[650px]">
                         <ShikiHighlighter
                             theme="min-light"
                             language={scopeql as LanguageRegistration}
                             showLanguage={false}
-                        >
+                            className='p-0 text-[14px] whitespace-pre-wrap'
+            >
                             {categories[activeTab].scopeql.trim()}
                         </ShikiHighlighter>
                     </div>
@@ -116,3 +117,4 @@ export default function ScopeQLVSSQL() {
         </div>
     );
 }
+
