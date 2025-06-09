2167c5446154e7f3a71fcf0926fd8e299eb8875c
CaiCai <hi@cai-cai.me>
2025-06-08 18:23:08 -0600

    style: adjust typography and spacing for improved readability across components

--- src/components/landing-page/ScopeQLVSSQL/index.tsx
+++ src/components/landing-page/ScopeQLVSSQL/index.tsx
@@ -38,28 +38,16 @@ SELECT last_name, school, salary`,
               FROM users`,
     sql: `SELECT *
           FROM users`
-}, {
-    title: "Aggregate on Windows",
-    scopeql: `SELECT *
-              FROM users`,
-    sql: `SELECT *
-          FROM users`
-}, {
-    title: "Joins",
-    scopeql: `SELECT *
-              FROM users`,
-    sql: `SELECT *
-          FROM users`
 }]
 
 export default function ScopeQLVSSQL() {
     const [activeTab, setActiveTab] = useState(0);
 
     return (
-        <div className="bg-[#F9F9F9] rounded-[16px] py-[16px] px-[16px] flex flex-col max-w-[720px] ">
+        <div className="bg-[#F9F9F9] rounded-[16px] p-[16px] flex flex-col w-full max-w-[720px] xl:max-w-[720px]">
             {/* Tab Navigation */}
             <div
-                className="flex text-[14px] flex-nowrap gap-[24px] overflow-x-auto px-[8px] text-[var(--color-secondary)]">
+                className="flex text-[14px] flex-nowrap gap-[16px] md:gap-[24px] overflow-x-auto text-[var(--color-secondary)]">
                 {categories.map((category, index) => (
                     <button
                         key={category.title}
@@ -101,7 +89,7 @@ export default function ScopeQLVSSQL() {
                     <div className="pb-[14px]">
                         <h3 className="text-[rgba(0,0,0,0.40)] text-[13px]">ScopeQL</h3>
                     </div>
-                    <div className="font-[14px] overflow-x-auto w-[650px]">
+                    <div className="font-[14px] overflow-x-auto w-full">
                         <ShikiHighlighter
                             theme="min-light"
                             language={scopeql as LanguageRegistration}
