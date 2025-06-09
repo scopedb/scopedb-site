ea2739b385b450a6452eb12cc2558b554a803403
tison <wander4096@gmail.com>
2025-06-09 22:45:37 +0800

    factor out callout.cs
    Signed-off-by: tison <wander4096@gmail.com>

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "ScopeDB";
-export const SITE_DESCRIPTION = "ScopeDB is a database that runs directly on top of any commodity object storage. It is designed explicitly for data workloads with massive writes, any-scale analysis, and flexible schema.";
+export const SiteName = "ScopeDB";
+export const SideDesc = "ScopeDB is a database that runs directly on top of any commodity object storage. It is designed explicitly for data workloads with massive writes, any-scale analysis, and flexible schema.";
 
-export const SUBMIT_CONTACT_API_URL = import.meta.env.PUBLIC_SUBMIT_CONTACT_API_URL;
-export const BOOKING_CALENDAR_URL = import.meta.env.PUBLIC_BOOKING_CALENDAR_URL;
+export const SubmitContactUrl = import.meta.env.PUBLIC_SUBMIT_CONTACT_API_URL;
+export const CalendarBookingUrl = import.meta.env.PUBLIC_BOOKING_CALENDAR_URL;
