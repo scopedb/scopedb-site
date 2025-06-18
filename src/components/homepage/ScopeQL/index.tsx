0526504f5474627d5e3f2ddb19d5359839dd8a65
tison <wander4096@gmail.com>
2025-06-18 16:02:51 +0800

    switch langauge show case order
    Signed-off-by: tison <wander4096@gmail.com>

--- src/components/homepage/ScopeQL/index.tsx
+++ src/components/homepage/ScopeQL/index.tsx
@@ -135,12 +135,54 @@ const categories = [{
     `
 }]
 
-export default function ScopeQLVSSQL() {
+function ScopeQLSection({activeTab}: { activeTab: number }) {
+    return <div className="flex-1 bg-[#FFFFFF]  rounded-[12px] p-[16px] mb-[12px]"
+         style={{
+             boxShadow: "0px 0px 8px 8px rgba(0, 0, 0, 0.02);"
+         }}>
+        <div className="pb-[14px]">
+            <h3 className="text-[var(--text-tertiary)] text-[13px]">ScopeQL</h3>
+        </div>
+        <div className="font-[14px] overflow-x-auto w-full">
+            <ShikiHighlighter
+                theme="min-light"
+                language={scopeql as LanguageRegistration}
+                showLanguage={false}
+                className='p-0 text-[14px] whitespace-pre-wrap'
+            >
+                {categories[activeTab].scopeql.trim()}
+            </ShikiHighlighter>
+        </div>
+    </div>
+}
+
+function SQLSection({activeTab}: { activeTab: number }) {
+    return <div className="flex-1 bg-[#FFFFFF] rounded-[16px] p-[20px]"
+         style={{
+             boxShadow: "0px 0px 8px 8px rgba(0, 0, 0, 0.02);"
+         }}>
+        <div className="pb-[14px]">
+            <h3 className="text-[var(--text-tertiary)] text-[13px]">SQL</h3>
+        </div>
+        <div className="font-[14px] overflow-x-auto w-full">
+            <ShikiHighlighter
+                theme="min-light"
+                language="sql"
+                showLanguage={false}
+                className='text-[14px] whitespace-pre-wrap p-0'
+
+            >
+                {categories[activeTab].sql.trim()}
+            </ShikiHighlighter>
+        </div>
+    </div>
+}
+
+export default function FeaturedScopeQL() {
     const [activeTab, setActiveTab] = useState(0);
 
     return (
         <div className="bg-[#F9F9F9] rounded-[16px] p-[16px] flex flex-col w-full max-w-[720px] xl:max-w-[720px]">
-            {/* Tab Navigation */}
             <div
                 className="flex text-[var(--text-primary)] text-[14px] flex-nowrap gap-[16px] md:gap-[24px] overflow-x-auto">
                 {categories.map((category, index) => (
@@ -155,47 +197,8 @@ export default function ScopeQLVSSQL() {
             </div>
 
             <div className="mt-[16px] flex flex-col flex-1">
-                {/* SQL Section */}
-                <div className="flex-1 bg-[#FFFFFF] rounded-[12px] p-[16px] mb-[12px]"
-                     style={{
-                         boxShadow: "0px 0px 8px 8px rgba(0, 0, 0, 0.02);"
-                     }}>
-                    <div className="pb-[14px]">
-                        <h3 className="text-[var(--text-tertiary)] text-[13px]">SQL</h3>
-                    </div>
-                    <div className="font-[14px] overflow-x-auto w-full">
-                        <ShikiHighlighter
-                            theme="min-light"
-                            language="sql"
-                            showLanguage={false}
-                            className='text-[14px] whitespace-pre-wrap p-0'
-
-                        >
-                            {categories[activeTab].sql.trim()}
-                        </ShikiHighlighter>
-                    </div>
-                </div>
-
-                {/* ScopeQL Section */}
-                <div className="flex-1 bg-[#FFFFFF] rounded-[16px] p-[20px]"
-                     style={{
-                         boxShadow: "0px 0px 8px 8px rgba(0, 0, 0, 0.02);"
-                     }}>
-                    <div className="pb-[14px]">
-                        <h3 className="text-[var(--text-tertiary)] text-[13px]">ScopeQL</h3>
-                    </div>
-                    <div className="font-[14px] overflow-x-auto w-full">
-                        <ShikiHighlighter
-                            theme="min-light"
-                            language={scopeql as LanguageRegistration}
-                            showLanguage={false}
-                            className='p-0 text-[14px] whitespace-pre-wrap'
-            >
-                            {categories[activeTab].scopeql.trim()}
-                        </ShikiHighlighter>
-                    </div>
-                </div>
-
+                <ScopeQLSection activeTab={activeTab}/>
+                <SQLSection activeTab={activeTab}/>
             </div>
         </div>
     );
