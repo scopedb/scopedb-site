2154ec66cd657b08031f0e03d9f1325abd434053
Zhiqiang ZHOU <im@strrl.dev>
2025-06-18 15:43:04 -0700

    refactor: ScopeDB site with Astro (#39)
    Signed-off-by: STRRL <im@strrl.dev>
    Signed-off-by: tison <wander4096@gmail.com>
    Co-authored-by: CaiCai <hi@cai-cai.me>
    Co-authored-by: tison <wander4096@gmail.com>

---
+++ src/components/calcom/FloatingButton.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { getCalApi } from "@calcom/embed-react";
+import { CalLink } from "@/src/consts";
+
+export default function FloatingButton() {
+    useEffect(() => {
+        (async function () {
+            const cal = await getCalApi();
+            cal("floatingButton", {
+                calLink: CalLink,
+                buttonText: "Book a demo",
+                config: {
+                    name: "John Smith",
+                    email: "john@company.com",
+                    theme: "light",
+                },
+            });
+            cal("ui", {
+                styles: {
+                    branding: { brandColor: "#000000" },
+                },
+            });
+        })();
+    }, []);
+
+    return <></>;
+}
---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SiteName = "ScopeDB";
+export const SideDesc = "ScopeDB is a database that runs directly on top of any commodity object storage. It is designed explicitly for data workloads with massive writes, any-scale analysis, and flexible schema.";
+
+export const ContactUrl = import.meta.env.PUBLIC_CONTACT_URL;
+export const CalLink = import.meta.env.PUBLIC_CAL_LINK;
