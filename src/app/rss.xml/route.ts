c2e2b51b6ac3ca87523e57c960e925b4a131cb49
Zhiqiang ZHOU <im@strrl.dev>
2025-07-22 08:57:16 -0700

    feat: fulfill rss content (#50)
    Signed-off-by: Zhiqiang ZHOU <im@strrl.dev>

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -1,29 +1,29 @@
 import RSS from 'rss'
+import { loadBlogContentByCategory } from '@/utils/loader'
 
 export async function GET(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     _request: Request
 ) {
     const feed = new RSS({
         title: 'ScopeDB Blogs',
         description: 'ScopeDB blogs on engineering, product, and more.',
-        site_url: 'https://www.scopedb.com',
-        feed_url: `https://www.scopedb.com/feed.xml`,
+        site_url: 'https://scopedb.io',
+        feed_url: `https://scopedb.io/rss.xml`,
         copyright: `© 2024-${new Date().getFullYear()} ScopeDB`,
         language: 'en',
         pubDate: new Date(),
     })
 
-    // const posts = await loadBlogContentByCategory('all')
-    // posts.forEach(post => {
-    //     feed.item({
-    //         title: post.title,
-    //         description: post.description,
-    //         url: `https://www.scopedb.com/blog/${post.slug}`,
-    //         date: post.pubDate,
-    //         categories: [post.category],
-    //     })
-    // })
+    const posts = await loadBlogContentByCategory('all')
+    posts.forEach(post => {
+        feed.item({
+            title: post.title,
+            description: post.description,
+            url: `https://scopedb.io/blog/${post.slug}`,
+            date: post.pubDate,
+            categories: [post.category],
+        })
+    })
 
     return new Response(feed.xml({ indent: true }), {
         headers: {
